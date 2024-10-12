53bad001a10415c2bef88e3af598cac8a6bcea9d
Md. Ataur Rahaman <ataurrahamandc@gmail.com>
2024-10-12 19:23:43 +0600

    seventh

--- js/theme.js
+++ js/theme.js
@@ -9,6 +9,11 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   let navBarWrapper = document.getElementById("navbar-wrapper");
+ 
+ 
+ 
+  
+  
   if ( document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
    
     navBarWrapper.style.paddingTop = "8px ";
@@ -25,29 +30,45 @@ function scrollFunction() {
 }
   }
 
- 
-
-  function menu() {
-    let navManuWrapper = document.getElementById("nav-manu-wrapper");
-    let closeMenu = document.getElementById("close-menu-icon");
-    let hamBar = document.getElementById("hambar")
-    if (navManuWrapper.className=== "nav-list-wrapper") {
-      navManuWrapper.className =("open-manu")
-      hamBar.style.display=("none");
-      closeMenu.style.display=("block")
-     
-           
-          
-    } else {
-
-     navManuWrapper.className ="nav-list-wrapper"
-     hamBar.style.display=("block");
-      closeMenu.style.display=("none")
-                  
+  if (window.innerWidth < 1139){
+    function menu() {
+      let navManuWrapper = document.getElementById("nav-manu-wrapper");
+      let closeMenu = document.getElementById("close-menu-icon");
+      let hamBar = document.getElementById("hambar");
+      if (navManuWrapper.className=== "nav-list-wrapper") {
+        navManuWrapper.className =("open-manu")
+        hamBar.style.display=("none");
+        closeMenu.style.display=("block")
+       
+             
+            
+      } else {
+  
+       navManuWrapper.className ="nav-list-wrapper"
+       hamBar.style.display=("block");
+        closeMenu.style.display=("none")
+                    
+      }
+  
+      
+         
     }
 
-    
-       
+    window.onscroll = function() {
+      let navManuWrapper = document.getElementById("nav-manu-wrapper");
+      let closeMenu = document.getElementById("close-menu-icon");
+      let hamBar = document.getElementById("hambar");
+      navManuWrapper.className ="nav-list-wrapper"
+      hamBar.style.display=("block");
+       closeMenu.style.display=("none");
+
+    };
+   
+  
   }
+
+  
+
+  
   
   
