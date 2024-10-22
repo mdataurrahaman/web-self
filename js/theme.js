0876574481170f02745682c21271406ea8070a22
Md. Ataur Rahaman <ataurrahamandc@gmail.com>
2024-10-23 00:04:14 +0600

    221024

--- js/theme.js
+++ js/theme.js
@@ -1,74 +1,39 @@
-
-
-
-
-
-  if (window.innerWidth > 1139) {
-   
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  let navBarWrapper = document.getElementById("navbar-wrapper");
- 
- 
- 
-  
-  
-  if ( document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-   
-    navBarWrapper.style.paddingTop = "8px ";
-    navBarWrapper.style.paddingBottom = "8px ";
-   navBarWrapper.style.borderBottomColor="#f3f3f3"
-
-  } else {
-    navBarWrapper.style.paddingTop = "24px ";
-    navBarWrapper.style.paddingBottom = "24px ";
-    navBarWrapper.style.borderBottomColor="transparent";
-    
-    
+if (window.innerWidth > 1024) {
+  window.onscroll = function () { scrollFunction() };
+  function scrollFunction() {
+    let navBarWrapper = document.getElementById("navbar-wrapper");
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+      navBarWrapper.style.paddingTop = "8px ";
+      navBarWrapper.style.paddingBottom = "8px ";
+      navBarWrapper.style.borderBottomColor = "#f3f3f3"
+    } else {
+      navBarWrapper.style.paddingTop = "24px ";
+      navBarWrapper.style.paddingBottom = "24px ";
+      navBarWrapper.style.borderBottomColor = "transparent";
+    }
   }
 }
-  }
-
-  if (window.innerWidth < 1139){
-    function menu() {
-      let navManuWrapper = document.getElementById("nav-manu-wrapper");
-      let closeMenu = document.getElementById("close-menu-icon");
-      let hamBar = document.getElementById("hambar");
-      if (navManuWrapper.className=== "nav-list-wrapper") {
-        navManuWrapper.className =("open-manu")
-        hamBar.style.display=("none");
-        closeMenu.style.display=("block")
-       
-             
-            
-      } else {
-  
-       navManuWrapper.className ="nav-list-wrapper"
-       hamBar.style.display=("block");
-        closeMenu.style.display=("none")
-                    
-      }
-  
-      
-         
+if (window.innerWidth < 1024) {
+  function menu() {
+    let navManuWrapper = document.getElementById("nav-manu-wrapper");
+    let closeMenu = document.getElementById("close-menu-icon");
+    let hamBar = document.getElementById("hambar");
+    if (navManuWrapper.className === "nav-list-wrapper") {
+      navManuWrapper.className = ("open-manu")
+      hamBar.style.display = ("none");
+      closeMenu.style.display = ("block")
+    } else {
+      navManuWrapper.className = "nav-list-wrapper"
+      hamBar.style.display = ("block");
+      closeMenu.style.display = ("none")
     }
-
-    window.onscroll = function() {
-      let navManuWrapper = document.getElementById("nav-manu-wrapper");
-      let closeMenu = document.getElementById("close-menu-icon");
-      let hamBar = document.getElementById("hambar");
-      navManuWrapper.className ="nav-list-wrapper"
-      hamBar.style.display=("block");
-       closeMenu.style.display=("none");
-
-    };
-   
-  
   }
-
-  
-
-  
-  
-  
+  window.onscroll = function () {
+    let navManuWrapper = document.getElementById("nav-manu-wrapper");
+    let closeMenu = document.getElementById("close-menu-icon");
+    let hamBar = document.getElementById("hambar");
+    navManuWrapper.className = "nav-list-wrapper"
+    hamBar.style.display = ("block");
+    closeMenu.style.display = ("none");
+  };
+}
